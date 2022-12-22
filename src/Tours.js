18a47d1064d209e1586add8c7aef7f5373dc7f80
john smilga <johnsmilga@gmail.com>
2022-12-21 20:14:31 -0500

    switch to three column layout

--- src/Tours.js
+++ src/Tours.js
@@ -3,11 +3,11 @@ import Tour from './Tour';
 const Tours = ({ tours, removeTour }) => {
   return (
     <section>
-      <div className="title">
+      <div className='title'>
         <h2>our tours</h2>
-        <div className="underline"></div>
+        <div className='underline'></div>
       </div>
-      <div>
+      <div className='tours'>
         {tours.map((tour) => {
           return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
         })}
